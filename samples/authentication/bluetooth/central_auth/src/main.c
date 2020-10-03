2574d0e86cf6da6bc53221025425d2c900f5424e
Dean <deang@goldenbits.com>
2020-10-03 16:05:10 -0700

    Fixed spelling error.

--- samples/authentication/bluetooth/central_auth/src/main.c
+++ samples/authentication/bluetooth/central_auth/src/main.c
@@ -558,7 +558,7 @@ void main(void)
     uint32_t flags = AUTH_CONN_CLIENT;
 
 #if defined(CONFIG_AUTH_DTLS) && defined(CONFIG_AUTH_CHALLENGE_RESPONSE)
-#error Invalid authenticaiton config, either DTLS or Challenge-Response, not both.
+#error Invalid authentication config, either DTLS or Challenge-Response, not both.
 #endif
 
 #if defined(CONFIG_AUTH_DTLS)
